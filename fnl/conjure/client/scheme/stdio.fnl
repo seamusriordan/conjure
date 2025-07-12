44466a636332d74c7ae8d26e202691838c3a2372
Seamus Riordan <seamus@riordan.dev>
2025-07-12 12:12:21 -0500

    Use local M export convention for scheme client

--- fnl/conjure/client/scheme/stdio.fnl
+++ fnl/conjure/client/scheme/stdio.fnl
@@ -1,4 +1,4 @@
-(local {: autoload} (require :conjure.nfnl.module))
+(local {: autoload : define} (require :conjure.nfnl.module))
 (local a (autoload :conjure.aniseed.core))
 (local str (autoload :conjure.aniseed.string))
 (local stdio (autoload :conjure.remote.stdio))
@@ -9,6 +9,8 @@
 (local log (autoload :conjure.log))
 (local ts (autoload :conjure.tree-sitter))
 (local cmpl (autoload :conjure.client.scheme.completions))
+
+(local M (define :conjure.client.scheme.stdio))
 
 (config.merge
   {:client
@@ -31,22 +33,23 @@
 
 (local cfg (config.get-in-fn [:client :scheme :stdio]))
 (local state (client.new-state #(do {:repl nil})))
-(local buf-suffix ".scm")
-(local comment-prefix "; ")
-(local form-node? ts.node-surrounded-by-form-pair-chars?)
+
+(set M.buf-suffix ".scm")
+(set M.comment-prefix "; ")
+(set M.form-node? ts.node-surrounded-by-form-pair-chars?)
 
 (fn with-repl-or-warn [f opts]
   (let [repl (state :repl)]
     (if repl
       (f repl)
-      (log.append [(.. comment-prefix "No REPL running")]))))
+      (log.append [(.. M.comment-prefix "No REPL running")]))))
 
-(fn unbatch [msgs]
+(fn M.unbatch [msgs]
   {:out (->> msgs
           (a.map #(or (a.get $1 :out) (a.get $1 :err)))
           (str.join ""))})
 
-(fn format-msg [msg]
+(fn M.format-msg [msg]
   (->> (-> msg
            (a.get :out)
            (string.gsub "^%s*" "")
@@ -61,41 +64,41 @@
              (string.match line (cfg [:value_prefix_pattern]))
              (string.gsub line (cfg [:value_prefix_pattern]) "")
 
-             (.. comment-prefix "(out) " line))))
+             (.. M.comment-prefix "(out) " line))))
        (a.filter #(not (str.blank? $1)))))
 
-(fn eval-str [opts]
+(fn M.eval-str [opts]
   (with-repl-or-warn
     (fn [repl]
       (repl.send
         (.. opts.code "\n")
         (fn [msgs]
-          (let [msgs (-> msgs unbatch format-msg)]
+          (let [msgs (-> msgs M.unbatch M.format-msg)]
             (opts.on-result (a.last msgs))
             (log.append msgs)))
         {:batch? true}))))
 
-(fn eval-file [opts]
-  (eval-str (a.assoc opts :code (.. "(load \"" opts.file-path "\")"))))
+(fn M.eval-file [opts]
+  (M.eval-str (a.assoc opts :code (.. "(load \"" opts.file-path "\")"))))
 
 (fn display-repl-status [status]
   (log.append
-    [(.. comment-prefix
+    [(.. M.comment-prefix
          (cfg [:command])
          " (" (or status "no status") ")")]
     {:break? true}))
 
-(fn stop []
+(fn M.stop []
   (let [repl (state :repl)]
     (when repl
       (repl.destroy)
       (display-repl-status :stopped)
       (a.assoc (state) :repl nil))))
 
-(fn start []
+(fn M.start []
   (if (state :repl)
-    (log.append [(.. comment-prefix "Can't start, REPL is already running.")
-                 (.. comment-prefix "Stop the REPL with "
+    (log.append [(.. M.comment-prefix "Can't start, REPL is already running.")
+                 (.. M.comment-prefix "Stop the REPL with "
                      (config.get-in [:mapping :prefix])
                      (cfg [:mapping :stop]))]
                 {:break? true})
@@ -116,47 +119,47 @@
          :on-exit
          (fn [code signal]
            (when (and (= :number (type code)) (> code 0))
-             (log.append [(.. comment-prefix "process exited with code " code)]))
+             (log.append [(.. M.comment-prefix "process exited with code " code)]))
            (when (and (= :number (type signal)) (> signal 0))
-             (log.append [(.. comment-prefix "process exited with signal " signal)]))
-           (stop))
+             (log.append [(.. M.comment-prefix "process exited with signal " signal)]))
+           (M.stop))
 
          :on-stray-output
          (fn [msg]
-           (log.append (format-msg msg)))}))))
+           (log.append (M.format-msg msg)))}))))
 
-(fn interrupt []
+(fn M.interrupt []
   (with-repl-or-warn
     (fn [repl]
-      (log.append [(.. comment-prefix " Sending interrupt signal.")] {:break? true})
+      (log.append [(.. M.comment-prefix " Sending interrupt signal.")] {:break? true})
       (repl.send-signal :sigint))))
 
-(fn on-load []
-  (start))
+(fn M.on-load []
+  (M.start))
 
-(fn on-filetype []
+(fn M.on-filetype []
   (mapping.buf
     :SchemeStart (cfg [:mapping :start])
-    start
+    M.start
     {:desc "Start the REPL"})
 
   (mapping.buf
     :SchemeStop (cfg [:mapping :stop])
-    stop
+    M.stop
     {:desc "Stop the REPL"})
 
   (mapping.buf
     :SchemeInterrupt (cfg [:mapping :interrupt])
-    interrupt
+    M.interrupt
     {:desc "Interrupt the REPL"}))
 
-(fn on-exit []
-  (stop))
+(fn M.on-exit []
+  (M.stop))
 
 (fn completions-enabled? []
   (cfg [:enable_completions]))
 
-(fn completions [opts]
+(fn M.completions [opts]
   ;(when (not= nil opts)
   ;  (log.append [(.. "; completions() called with: " (a.pr-str opts))] {:break? true}))
   (if (completions-enabled?)
@@ -167,17 +170,4 @@
       (opts.cb suggestions))
     (opts.cb [])))
 
-{: buf-suffix
- : comment-prefix
- : form-node?
- : unbatch
- : format-msg
- : eval-str
- : eval-file
- : stop
- : start
- : interrupt
- : on-load
- : on-filetype
- : on-exit 
- : completions}
+M
--- lua/conjure/client/scheme/stdio.lua
+++ lua/conjure/client/scheme/stdio.lua
@@ -1,6 +1,7 @@
 -- [nfnl] fnl/conjure/client/scheme/stdio.fnl
 local _local_1_ = require("conjure.nfnl.module")
 local autoload = _local_1_["autoload"]
+local define = _local_1_["define"]
 local a = autoload("conjure.aniseed.core")
 local str = autoload("conjure.aniseed.string")
 local stdio = autoload("conjure.remote.stdio")
@@ -11,6 +12,7 @@ local client = autoload("conjure.client")
 local log = autoload("conjure.log")
 local ts = autoload("conjure.tree-sitter")
 local cmpl = autoload("conjure.client.scheme.completions")
+local M = define("conjure.client.scheme.stdio")
 config.merge({client = {scheme = {stdio = {command = "mit-scheme", prompt_pattern = "[%]e][=r]r?o?r?> ", value_prefix_pattern = "^;Value: ", enable_completions = true}}}})
 if config["get-in"]({"mapping", "enable_defaults"}) then
   config.merge({client = {scheme = {stdio = {mapping = {start = "cs", stop = "cS", interrupt = "ei"}}}}})
@@ -22,24 +24,24 @@ local function _3_()
   return {repl = nil}
 end
 state = client["new-state"](_3_)
-local buf_suffix = ".scm"
-local comment_prefix = "; "
-local form_node_3f = ts["node-surrounded-by-form-pair-chars?"]
+M["buf-suffix"] = ".scm"
+M["comment-prefix"] = "; "
+M["form-node?"] = ts["node-surrounded-by-form-pair-chars?"]
 local function with_repl_or_warn(f, opts)
   local repl = state("repl")
   if repl then
     return f(repl)
   else
-    return log.append({(comment_prefix .. "No REPL running")})
+    return log.append({(M["comment-prefix"] .. "No REPL running")})
   end
 end
-local function unbatch(msgs)
+M.unbatch = function(msgs)
   local function _5_(_241)
     return (a.get(_241, "out") or a.get(_241, "err"))
   end
   return {out = str.join("", a.map(_5_, msgs))}
 end
-local function format_msg(msg)
+M["format-msg"] = function(msg)
   local function _6_(_241)
     return not str["blank?"](_241)
   end
@@ -49,15 +51,15 @@ local function format_msg(msg)
     elseif string.match(line, cfg({"value_prefix_pattern"})) then
       return string.gsub(line, cfg({"value_prefix_pattern"}), "")
     else
-      return (comment_prefix .. "(out) " .. line)
+      return (M["comment-prefix"] .. "(out) " .. line)
     end
   end
   return a.filter(_6_, a.map(_7_, str.split(string.gsub(string.gsub(a.get(msg, "out"), "^%s*", ""), "%s+%d+%s*$", ""), "\n")))
 end
-local function eval_str(opts)
+M["eval-str"] = function(opts)
   local function _9_(repl)
     local function _10_(msgs)
-      local msgs0 = format_msg(unbatch(msgs))
+      local msgs0 = M["format-msg"](M.unbatch(msgs))
       opts["on-result"](a.last(msgs0))
       return log.append(msgs0)
     end
@@ -65,13 +67,13 @@ local function eval_str(opts)
   end
   return with_repl_or_warn(_9_)
 end
-local function eval_file(opts)
-  return eval_str(a.assoc(opts, "code", ("(load \"" .. opts["file-path"] .. "\")")))
+M["eval-file"] = function(opts)
+  return M["eval-str"](a.assoc(opts, "code", ("(load \"" .. opts["file-path"] .. "\")")))
 end
 local function display_repl_status(status)
-  return log.append({(comment_prefix .. cfg({"command"}) .. " (" .. (status or "no status") .. ")")}, {["break?"] = true})
+  return log.append({(M["comment-prefix"] .. cfg({"command"}) .. " (" .. (status or "no status") .. ")")}, {["break?"] = true})
 end
-local function stop()
+M.stop = function()
   local repl = state("repl")
   if repl then
     repl.destroy()
@@ -81,9 +83,9 @@ local function stop()
     return nil
   end
 end
-local function start()
+M.start = function()
   if state("repl") then
-    return log.append({(comment_prefix .. "Can't start, REPL is already running."), (comment_prefix .. "Stop the REPL with " .. config["get-in"]({"mapping", "prefix"}) .. cfg({"mapping", "stop"}))}, {["break?"] = true})
+    return log.append({(M["comment-prefix"] .. "Can't start, REPL is already running."), (M["comment-prefix"] .. "Stop the REPL with " .. config["get-in"]({"mapping", "prefix"}) .. cfg({"mapping", "stop"}))}, {["break?"] = true})
   else
     local function _12_()
       return display_repl_status("started")
@@ -93,43 +95,43 @@ local function start()
     end
     local function _14_(code, signal)
       if (("number" == type(code)) and (code > 0)) then
-        log.append({(comment_prefix .. "process exited with code " .. code)})
+        log.append({(M["comment-prefix"] .. "process exited with code " .. code)})
       else
       end
       if (("number" == type(signal)) and (signal > 0)) then
-        log.append({(comment_prefix .. "process exited with signal " .. signal)})
+        log.append({(M["comment-prefix"] .. "process exited with signal " .. signal)})
       else
       end
-      return stop()
+      return M.stop()
     end
     local function _17_(msg)
-      return log.append(format_msg(msg))
+      return log.append(M["format-msg"](msg))
     end
     return a.assoc(state(), "repl", stdio.start({["prompt-pattern"] = cfg({"prompt_pattern"}), cmd = cfg({"command"}), ["on-success"] = _12_, ["on-error"] = _13_, ["on-exit"] = _14_, ["on-stray-output"] = _17_}))
   end
 end
-local function interrupt()
+M.interrupt = function()
   local function _19_(repl)
-    log.append({(comment_prefix .. " Sending interrupt signal.")}, {["break?"] = true})
+    log.append({(M["comment-prefix"] .. " Sending interrupt signal.")}, {["break?"] = true})
     return repl["send-signal"]("sigint")
   end
   return with_repl_or_warn(_19_)
 end
-local function on_load()
-  return start()
+M["on-load"] = function()
+  return M.start()
 end
-local function on_filetype()
-  mapping.buf("SchemeStart", cfg({"mapping", "start"}), start, {desc = "Start the REPL"})
-  mapping.buf("SchemeStop", cfg({"mapping", "stop"}), stop, {desc = "Stop the REPL"})
-  return mapping.buf("SchemeInterrupt", cfg({"mapping", "interrupt"}), interrupt, {desc = "Interrupt the REPL"})
+M["on-filetype"] = function()
+  mapping.buf("SchemeStart", cfg({"mapping", "start"}), M.start, {desc = "Start the REPL"})
+  mapping.buf("SchemeStop", cfg({"mapping", "stop"}), M.stop, {desc = "Stop the REPL"})
+  return mapping.buf("SchemeInterrupt", cfg({"mapping", "interrupt"}), M.interrupt, {desc = "Interrupt the REPL"})
 end
-local function on_exit()
-  return stop()
+M["on-exit"] = function()
+  return M.stop()
 end
 local function completions_enabled_3f()
   return cfg({"enable_completions"})
 end
-local function completions(opts)
+M.completions = function(opts)
   if completions_enabled_3f() then
     local all_completions = cmpl["get-completions"]()
     local prefix_pattern = ("^" .. opts.prefix)
@@ -144,4 +146,4 @@ local function completions(opts)
     return opts.cb({})
   end
 end
-return {["buf-suffix"] = buf_suffix, ["comment-prefix"] = comment_prefix, ["form-node?"] = form_node_3f, unbatch = unbatch, ["format-msg"] = format_msg, ["eval-str"] = eval_str, ["eval-file"] = eval_file, stop = stop, start = start, interrupt = interrupt, ["on-load"] = on_load, ["on-filetype"] = on_filetype, ["on-exit"] = on_exit, completions = completions}
+return M
